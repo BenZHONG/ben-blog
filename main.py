c34a7999a9f51fc96e55add37491db3f8b3d6f92
zzl <zhiliangzhong@gmail.com>
2022-07-14 11:23:59 +1000

    Change SQLite to PostgreSQL

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ https://pythonhosted.org/Flask-Gravatar/
 pip install Flask-Gravatar
 """
 
+import os
+
 from flask import Flask, render_template, redirect, url_for, flash, abort
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
@@ -18,12 +20,14 @@ from functools import wraps
 from flask import g, request
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+# app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")  # PostgreSQL
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'  # SQLite
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
